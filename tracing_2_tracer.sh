39a07a3e840ba3efe73410a4c849cbf942d9994d
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2022-10-31 12:07:04 -0500

    Fail on incorrect corpus path

--- tracing_2_tracer.sh
+++ tracing_2_tracer.sh
@@ -5,4 +5,9 @@ PROJECT_NAME="$1"
 FUZZER="$2"
 CORPUS_DIR="$3"
 
+if [ ! -d $CORPUS_DIR ]
+then
+    exec ls $CORPUS_DIR
+fi
+
 python3 infra/helper.py reproduce --tracer --num_runs 1 $PROJECT_NAME $FUZZER $CORPUS_DIR timeout=300
