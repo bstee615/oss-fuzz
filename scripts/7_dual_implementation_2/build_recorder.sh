ba82cf18e9551c323c62e36240387bba7efab640
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2023-01-09 16:16:23 -0600

    Quit build on errro

--- scripts/7_dual_implementation_2/build_recorder.sh
+++ scripts/7_dual_implementation_2/build_recorder.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 (cd recorder && ./gradlew build jar)
 cp recorder/lib/build/libs/recorder-1.0.0.jar infra/base-images/base-builder-jvm/recorder-1.0.0.jar
 cp recorder/lib/build/libs/recorder-1.0.0.jar infra/base-images/base-runner/recorder-1.0.0.jar
