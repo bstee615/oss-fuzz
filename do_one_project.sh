c4d4c90231f6239d4e340d80c301fdd864a1335c
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2023-01-09 15:09:04 -0600

    Add script to run one example tracing dual encoder

--- do_one_project.sh
+++ do_one_project.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-PROJECT="$1"
-FUZZER="$2"
-LOG_NAME="$3"
+# PROJECT="$1"
+# FUZZER="$2"
+# LOG_NAME="$3"
+PROJECT="angus-mail"
+FUZZER="ASCIIUtilityFuzzer"
+LOG_NAME="do_one_test/log"
+
 TXT_FILE="$LOG_NAME.txt"
 JSON_FILE="$LOG_NAME.json"
 TRACER_LOG_FILE="${LOG_NAME}_tracer.txt"
@@ -28,27 +32,16 @@ function build_fuzzer() {
 git checkout projects/$PROJECT
 echo "Instrumenting $PROJECT $FUZZER..."
 python modify_build_scripts.py $PROJECT
-java -jar transformer/build/libs/transformer-1.0-SNAPSHOT.jar --mode INSTRUMENT --fuzzerDir projects/$PROJECT/ --fuzzerName $FUZZER
+java -jar transformer/build/libs/transformer-1.0-SNAPSHOT.jar --mode INSTRUMENT --fuzzerName $FUZZER --baseLoggingDir /recorder --write --fuzzerDir projects/$PROJECT/ --jarDir build/out/$PROJECT/
 build_fuzzer $PROJECT
 
 # Run with tracer
-echo "Tracing $PROJECT $FUZZER -> $JSON_FILE"
-(python3 infra/helper.py reproduce --tracer --tracer_port 8000 --num_runs 1 --container_name foo $PROJECT $FUZZER \
-    $CORPUS_ROOT/$PROJECT/address-x86_64-$FUZZER timeout=300 instrumentation_excludes="**" 2>&1 \
-    | tee $TXT_FILE \
-    | bash print_fuzzeroutput.sh | jq --slurp) > $JSON_FILE &
-sleep 10s
-(java -jar ../trace-modeling/trace_collection_java/app/build/libs/tracer.jar \
-    -t dt_socket -p 8000 -m fuzzerTestOneInput -v DEBUG \
-    -l $XML_FILE 2>&1 | tee $TRACER_LOG_FILE) &
-wait
-
-# Postprocess results
-python transform_xml_to_callresults.py $XML_FILE $RESULTS_JSON_FILE
-read -p "MODIFY HARNESS using $RESULTS_JSON_FILE, then Press enter to continue"
+echo "Running $PROJECT $FUZZER -> $JSON_FILE"
+python3 infra/helper.py reproduce --num_runs 1 --container_name foo $PROJECT $FUZZER \
+    $(ls -d $CORPUS_ROOT/$PROJECT/address-x86_64-$FUZZER/* | head -n1) timeout=300 instrumentation_excludes="**"
 
 # Reset, then hardcode fuzzer
-git checkout projects/$PROJECT
-echo "Hardcoding inputs $PROJECT $FUZZER..."
-java -jar transformer/build/libs/transformer-1.0-SNAPSHOT.jar --mode HARDCODE --fuzzerDir projects/$PROJECT/ --fuzzerName $FUZZER --jsonFile $JSON_FILE --resultFile $RESULTS_JSON_FILE
-build_fuzzer $PROJECT
+# git checkout projects/$PROJECT
+# echo "Hardcoding inputs $PROJECT $FUZZER..."
+# java -jar transformer/build/libs/transformer-1.0-SNAPSHOT.jar --mode HARDCODE --fuzzerDir projects/$PROJECT/ --fuzzerName $FUZZER --jsonFile $JSON_FILE --resultFile $RESULTS_JSON_FILE
+# build_fuzzer $PROJECT
