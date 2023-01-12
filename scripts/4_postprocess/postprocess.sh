73c5b83e35ba451b2b0b5a1d851b49b325846a84
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2023-01-12 14:24:41 -0600

    Run postprocessing again

--- scripts/4_postprocess/postprocess.sh
+++ scripts/4_postprocess/postprocess.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-SAMPLE="--sample"
+# SAMPLE="--sample"
 # SINGLE_THREAD="--single_thread"
 
-# SAMPLE=""
+SAMPLE=""
 SINGLE_THREAD=""
 
 NPROC=6
 
-INDIR="postprocessed_xmls/"
+INDIR="repaired_xmls/logs-xmls"
 OUTFILE="postprocessed.jsonl"
 python scripts/4_postprocess/2_main.py $INDIR $OUTFILE $SAMPLE $SINGLE_THREAD --nproc $NPROC
 
@@ -20,4 +20,4 @@ INFILE="$OUTFILE"
 OUTFILE="${INFILE%.jsonl}_sort.jsonl"
 python scripts/4_postprocess/4_sort_examples.py $INFILE $OUTFILE
 
-python scripts/4_postprocess/8_filter_variables.py
+python scripts/4_postprocess/8_filter_variables.py $OUTFILE
