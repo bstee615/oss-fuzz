bd20336e0de5104a522d116fc5ed93087e3a84e6
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2023-01-13 16:29:08 -0600

    Output log to file

--- scripts/4_postprocess/postprocess.sh
+++ scripts/4_postprocess/postprocess.sh
@@ -3,21 +3,19 @@
 # SAMPLE="--sample"
 # SINGLE_THREAD="--single_thread"
 
-SAMPLE=""
-SINGLE_THREAD=""
-
-NPROC=6
+NPROC=10
+LOGFILE=postprocess.log
 
 INDIR="repaired_xmls/logs-xmls"
 OUTFILE="postprocessed.jsonl"
-python scripts/4_postprocess/2_main.py $INDIR $OUTFILE $SAMPLE $SINGLE_THREAD --nproc $NPROC
+python scripts/4_postprocess/2_main.py $INDIR $OUTFILE $SAMPLE $SINGLE_THREAD --nproc $NPROC 2>&1 | tee $LOGFILE
 
 INFILE="$OUTFILE"
 OUTFILE="${INFILE%.jsonl}_dedup.jsonl"
-python scripts/4_postprocess/3_dedup_filter_examples.py $INFILE $OUTFILE
+python scripts/4_postprocess/3_dedup_filter_examples.py $INFILE $OUTFILE 2>&1 | tee -a $LOGFILE
 
 INFILE="$OUTFILE"
 OUTFILE="${INFILE%.jsonl}_sort.jsonl"
-python scripts/4_postprocess/4_sort_examples.py $INFILE $OUTFILE
+python scripts/4_postprocess/4_sort_examples.py $INFILE $OUTFILE 2>&1 | tee -a $LOGFILE
 
-python scripts/4_postprocess/8_filter_variables.py $OUTFILE
+python scripts/4_postprocess/8_filter_variables.py $OUTFILE 2>&1 | tee -a $LOGFILE
