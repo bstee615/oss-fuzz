70ea01644cd8365873e13182bd88d59d484ef48d
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2023-01-13 16:39:15 -0600

    Update dual encoder script

--- scripts/7_dual_implementation_2/do_one_project.sh
+++ scripts/7_dual_implementation_2/do_one_project.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-# PROJECT="$1"
-# FUZZER="$2"
-# LOG_NAME="$3"
-PROJECT="angus-mail"
-FUZZER="ASCIIUtilityFuzzer"
-LOG_NAME="do_one_test/log"
-
-TXT_FILE="$LOG_NAME.txt"
-JSON_FILE="$LOG_NAME.json"
-TRACER_LOG_FILE="${LOG_NAME}_tracer.txt"
-XML_FILE="$LOG_NAME.xml"
-RESULTS_JSON_FILE="${LOG_NAME}_results.json"
-HARDCODED_TXT_FILE="${LOG_NAME}_hardcoded.txt"
-HARDCODED_JSON_FILE="${LOG_NAME}_hardcoded.json"
-
-XML_FILE_2="${LOG_NAME}_2.xml"
-TRACER_LOG_FILE_2="${LOG_NAME}_tracer2.txt"
+PROJECT="$1"
+FUZZER="$2"
+# PROJECT="angus-mail"
+# FUZZER="ASCIIUtilityFuzzer"
 
 CORPUS_ROOT="/run/media/benjis/BASILISK/Files/biggie/oss-fuzz/fuzz_10m_trace_3h/fuzz_run_5_complete/corpora-10m"
+CORPUS_INPUT="$(ls -d $CORPUS_ROOT/$PROJECT/address-x86_64-$FUZZER/* | head -n1)"
+
+RECORDER_LOG_DIR="recorder_logs"
+mkdir -p $RECORDER_LOG_DIR/
 
 set -e
 set -x
@@ -39,21 +30,20 @@ java -jar transformer/build/libs/transformer-1.0-SNAPSHOT.jar \
 build_fuzzer $PROJECT
 
 # Run with tracer
-echo "Running $PROJECT $FUZZER -> $JSON_FILE"
-FIRST_CORPUS_INPUT="$(ls -d $CORPUS_ROOT/$PROJECT/address-x86_64-$FUZZER/* | head -n1)"
-rm recorder_logs/*.{jsonl,json}
-python3 infra/helper.py reproduce --num_runs 1 --container_name foo $PROJECT $FUZZER $FIRST_CORPUS_INPUT timeout=300 instrumentation_excludes="**"
-for f in recorder_logs/*.jsonl
+echo "Running $PROJECT $FUZZER with input $CORPUS_INPUT..."
+python3 infra/helper.py reproduce --num_runs 1 --container_name foo $PROJECT $FUZZER $CORPUS_INPUT timeout=300 instrumentation_excludes="**"
+for f in $RECORDER_LOG_DIR/*.jsonl
 do
     jq --slurp < $f > ${f%.jsonl}.json
 done
 
 # Reset, then hardcode fuzzer
+echo "Hardcoding $PROJECT $FUZZER with input from $RECORDER_LOG_DIR and running with input $CORPUS_INPUT..."
 git checkout projects/$PROJECT
 java -jar transformer/build/libs/transformer-1.0-SNAPSHOT.jar \
     --write \
     --mode HARDCODE --fuzzerName $FUZZER \
     --fuzzerDir projects/$PROJECT/ --jarDir build/out/$PROJECT/ --jarDir recorder/lib/build/libs \
-    --inputFile recorder_logs/fuzzerOutput_ASCIIUtilityFuzzer.json --resultFile recorder_logs/fuzzerResult_ASCIIUtilityFuzzer.json
+    --inputFile $RECORDER_LOG_DIR/fuzzerOutput_ASCIIUtilityFuzzer.json --resultFile $RECORDER_LOG_DIR/fuzzerResult_ASCIIUtilityFuzzer.json
 build_fuzzer $PROJECT
-python3 infra/helper.py reproduce --num_runs 1 --container_name foo $PROJECT $FUZZER $FIRST_CORPUS_INPUT timeout=300 instrumentation_excludes="**"
+python3 infra/helper.py reproduce --num_runs 1 --container_name foo $PROJECT $FUZZER $CORPUS_INPUT timeout=300 instrumentation_excludes="**"
