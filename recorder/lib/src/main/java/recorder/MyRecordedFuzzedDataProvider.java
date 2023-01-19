89ec448669c13d1cf53b810503b80515c0e2073c
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2023-01-19 12:46:00 -0600

    Separate input recorder and wrapper

--- recorder/lib/src/main/java/recorder/MyRecordedFuzzedDataProvider.java
+++ recorder/lib/src/main/java/recorder/MyRecordedFuzzedDataProvider.java
@@ -10,325 +10,177 @@ import java.io.FileWriter;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.Base64;
-import java.lang.AutoCloseable;
 import java.lang.Thread;
 import java.lang.StackTraceElement;
 import com.google.gson.Gson;
 
 // Wraps the native FuzzedDataProviderImpl and serializes all its return values as JSONL.
-public final class MyRecordedFuzzedDataProvider implements FuzzedDataProvider, AutoCloseable {
+public final class MyRecordedFuzzedDataProvider implements FuzzedDataProvider {
   private FuzzedDataProvider provider;
-  private Gson gson;
-  private Writer writer;
-  private Writer resultWriter;
-  private int ordinal = -1;
-  String lastLocation = null;
+  private MyInputRecorder inputRecorder;
 
-  public MyRecordedFuzzedDataProvider(FuzzedDataProvider provider, String baseDir, String fuzzerTargetName) {
-    System.err.println(String.format("GREAT! Instrumented the class. provider.remainingBytes(): %d baseDir: \"%s\" fuzzerTargetName: \"%s\"", provider.remainingBytes(), baseDir, fuzzerTargetName));
+  public MyRecordedFuzzedDataProvider(FuzzedDataProvider provider, MyInputRecorder inputRecorder) {
     this.provider = provider;
-    String filename = String.format("%s/fuzzerOutput_%s.jsonl", baseDir, fuzzerTargetName);
-    String resultFilename = String.format("%s/fuzzerResult_%s.jsonl", baseDir, fuzzerTargetName);
-    try {
-        this.writer = new BufferedWriter(new FileWriter(filename, true));
-        this.resultWriter = new BufferedWriter(new FileWriter(resultFilename, true));
-    }
-    catch (IOException ex) {
-        System.err.printf("Error initializing writer: %s%n", filename);
-    }
-    this.gson = new Gson();
-  }
-
-  public void close() throws Exception {
-    this.writer.close();
-    this.resultWriter.close();
+    this.inputRecorder = inputRecorder;
   }
 
   public MyRecordedFuzzedDataProvider setLocation(String location) {
-    assert lastLocation == null;
-    lastLocation = location;
+    inputRecorder.setLocation(location);
     return this;
-  }
-
-  private void writeToFile(String string) {
-    System.err.println(String.format("WRITE TO FILE: \"%s\"", string));
-    try {
-        this.writer.append(string + "\n");
-    }
-    catch (IOException ex) {
-        System.err.printf("Error writing to file: %s%n", string);
-    }
-  }
-
-  private String encodeData(String type, String value) {
-    // StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace(); // NOTE: disable because no longer needed
-    // assert lastLocation != null;
-    String result = String.format("{\"type\": \"%s\", \"value\": %s, \"location\": %s}", type, value, lastLocation);
-    lastLocation = null;
-    return result;
-  }
-
-  private String encodeData(String type, int value) {
-    return encodeData(type, String.valueOf(value));
-  }
-
-  private String encodeData(String type) {
-    return encodeData(type, null);
-  }
-
-  /* END GENERATED METHODS */
-  private boolean recordAndReturn(boolean object) {
-    writeToFile(encodeData("boolean", gson.toJson(object)));
-    return object;
-  }
-
-  private boolean[] recordAndReturn(boolean[] object) {
-    writeToFile(encodeData("boolean[]", gson.toJson(object)));
-    return object;
-  }
-
-  private byte recordAndReturn(byte object) {
-    writeToFile(encodeData("byte", gson.toJson(object)));
-    return object;
-  }
-
-  private byte[] recordAndReturn(byte[] object) {
-    writeToFile(encodeData("byte[]", gson.toJson(object)));
-    return object;
-  }
-
-  private short recordAndReturn(short object) {
-    writeToFile(encodeData("short", gson.toJson(object)));
-    return object;
-  }
-
-  private short[] recordAndReturn(short[] object) {
-    writeToFile(encodeData("short[]", gson.toJson(object)));
-    return object;
-  }
-
-  private long recordAndReturn(long object) {
-    writeToFile(encodeData("long", gson.toJson(object)));
-    return object;
-  }
-
-  private long[] recordAndReturn(long[] object) {
-    writeToFile(encodeData("long[]", gson.toJson(object)));
-    return object;
-  }
-
-  private int recordAndReturn(int object) {
-    writeToFile(encodeData("int", gson.toJson(object)));
-    return object;
-  }
-
-  private int[] recordAndReturn(int[] object) {
-    writeToFile(encodeData("int[]", gson.toJson(object)));
-    return object;
-  }
-
-  private float recordAndReturn(float object) {
-    writeToFile(encodeData("float", gson.toJson(object)));
-    return object;
-  }
-
-  private float[] recordAndReturn(float[] object) {
-    writeToFile(encodeData("float[]", gson.toJson(object)));
-    return object;
-  }
-
-  private char recordAndReturn(char object) {
-    writeToFile(encodeData("char", gson.toJson(object)));
-    return object;
-  }
-
-  private char[] recordAndReturn(char[] object) {
-    writeToFile(encodeData("char[]", gson.toJson(object)));
-    return object;
-  }
-
-  private double recordAndReturn(double object) {
-    writeToFile(encodeData("double", gson.toJson(object)));
-    return object;
-  }
-
-  private double[] recordAndReturn(double[] object) {
-    writeToFile(encodeData("double[]", gson.toJson(object)));
-    return object;
-  }
-
-  private String recordAndReturn(String object) {
-    writeToFile(encodeData("String", gson.toJson(object)));
-    return object;
-  }
-
-  private int recordAndReturnCount(int object) {
-    writeToFile(encodeData("int_count", gson.toJson(object)));
-    return object;
-  }
-  /* END GENERATED METHODS */
-  
-  public void markBeginFuzzer() {
-    ordinal ++;
-    writeToFile(encodeData("begin", ordinal));
-  }
-  
-  public void markEndFuzzer() {
-    writeToFile(encodeData("end", ordinal));
   }
 
   @Override
   public boolean consumeBoolean() {
-    return recordAndReturn(provider.consumeBoolean());
+    return inputRecorder.recordAndReturn(provider.consumeBoolean());
   }
 
   @Override
   public boolean[] consumeBooleans(int maxLength) {
-    return recordAndReturn(provider.consumeBooleans(maxLength));
+    return inputRecorder.recordAndReturn(provider.consumeBooleans(maxLength));
   }
 
   @Override
   public byte consumeByte() {
-    return recordAndReturn(provider.consumeByte());
+    return inputRecorder.recordAndReturn(provider.consumeByte());
   }
 
   @Override
   public byte consumeByte(byte min, byte max) {
-    return recordAndReturn(provider.consumeByte(min, max));
+    return inputRecorder.recordAndReturn(provider.consumeByte(min, max));
   }
 
   @Override
   public byte[] consumeBytes(int maxLength) {
-    return recordAndReturn(provider.consumeBytes(maxLength));
+    return inputRecorder.recordAndReturn(provider.consumeBytes(maxLength));
   }
 
   @Override
   public byte[] consumeRemainingAsBytes() {
-    return recordAndReturn(provider.consumeRemainingAsBytes());
+    return inputRecorder.recordAndReturn(provider.consumeRemainingAsBytes());
   }
 
   @Override
   public short consumeShort() {
-    return recordAndReturn(provider.consumeShort());
+    return inputRecorder.recordAndReturn(provider.consumeShort());
   }
 
   @Override
   public short consumeShort(short min, short max) {
-    return recordAndReturn(provider.consumeShort(min, max));
+    return inputRecorder.recordAndReturn(provider.consumeShort(min, max));
   }
 
   @Override
   public short[] consumeShorts(int maxLength) {
-    return recordAndReturn(provider.consumeShorts(maxLength));
+    return inputRecorder.recordAndReturn(provider.consumeShorts(maxLength));
   }
 
   @Override
   public int consumeInt() {
-    return recordAndReturn(provider.consumeInt());
+    return inputRecorder.recordAndReturn(provider.consumeInt());
   }
 
   @Override
   public int consumeInt(int min, int max) {
-    return recordAndReturn(provider.consumeInt(min, max));
+    return inputRecorder.recordAndReturn(provider.consumeInt(min, max));
   }
 
   @Override
   public int[] consumeInts(int maxLength) {
-    return recordAndReturn(provider.consumeInts(maxLength));
+    return inputRecorder.recordAndReturn(provider.consumeInts(maxLength));
   }
 
   @Override
   public long consumeLong() {
-    return recordAndReturn(provider.consumeLong());
+    return inputRecorder.recordAndReturn(provider.consumeLong());
   }
 
   @Override
   public long consumeLong(long min, long max) {
-    return recordAndReturn(provider.consumeLong(min, max));
+    return inputRecorder.recordAndReturn(provider.consumeLong(min, max));
   }
 
   @Override
   public long[] consumeLongs(int maxLength) {
-    return recordAndReturn(provider.consumeLongs(maxLength));
+    return inputRecorder.recordAndReturn(provider.consumeLongs(maxLength));
   }
 
   @Override
   public float consumeFloat() {
-    return recordAndReturn(provider.consumeFloat());
+    return inputRecorder.recordAndReturn(provider.consumeFloat());
   }
 
   @Override
   public float consumeRegularFloat() {
-    return recordAndReturn(provider.consumeRegularFloat());
+    return inputRecorder.recordAndReturn(provider.consumeRegularFloat());
   }
 
   @Override
   public float consumeRegularFloat(float min, float max) {
-    return recordAndReturn(provider.consumeRegularFloat(min, max));
+    return inputRecorder.recordAndReturn(provider.consumeRegularFloat(min, max));
   }
 
   @Override
   public float consumeProbabilityFloat() {
-    return recordAndReturn(provider.consumeProbabilityFloat());
+    return inputRecorder.recordAndReturn(provider.consumeProbabilityFloat());
   }
 
   @Override
   public double consumeDouble() {
-    return recordAndReturn(provider.consumeDouble());
+    return inputRecorder.recordAndReturn(provider.consumeDouble());
   }
 
   @Override
   public double consumeRegularDouble() {
-    return recordAndReturn(provider.consumeRegularDouble());
+    return inputRecorder.recordAndReturn(provider.consumeRegularDouble());
   }
 
   @Override
   public double consumeRegularDouble(double min, double max) {
-    return recordAndReturn(provider.consumeRegularDouble(min, max));
+    return inputRecorder.recordAndReturn(provider.consumeRegularDouble(min, max));
   }
 
   @Override
   public double consumeProbabilityDouble() {
-    return recordAndReturn(provider.consumeProbabilityDouble());
+    return inputRecorder.recordAndReturn(provider.consumeProbabilityDouble());
   }
 
   @Override
   public char consumeChar() {
-    return recordAndReturn(provider.consumeChar());
+    return inputRecorder.recordAndReturn(provider.consumeChar());
   }
 
   @Override
   public char consumeChar(char min, char max) {
-    return recordAndReturn(provider.consumeChar(min, max));
+    return inputRecorder.recordAndReturn(provider.consumeChar(min, max));
   }
 
   @Override
   public char consumeCharNoSurrogates() {
-    return recordAndReturn(provider.consumeCharNoSurrogates());
+    return inputRecorder.recordAndReturn(provider.consumeCharNoSurrogates());
   }
 
   @Override
   public String consumeString(int maxLength) {
-    return recordAndReturn(provider.consumeString(maxLength));
+    return inputRecorder.recordAndReturn(provider.consumeString(maxLength));
   }
 
   @Override
   public String consumeRemainingAsString() {
-    return recordAndReturn(provider.consumeRemainingAsString());
+    return inputRecorder.recordAndReturn(provider.consumeRemainingAsString());
   }
 
   @Override
   public String consumeAsciiString(int maxLength) {
-    return recordAndReturn(provider.consumeAsciiString(maxLength));
+    return inputRecorder.recordAndReturn(provider.consumeAsciiString(maxLength));
   }
 
   @Override
   public String consumeRemainingAsAsciiString() {
-    return recordAndReturn(provider.consumeRemainingAsAsciiString());
+    return inputRecorder.recordAndReturn(provider.consumeRemainingAsAsciiString());
   }
 
   @Override
   public int remainingBytes() {
-    return recordAndReturnCount(provider.remainingBytes());
+    return inputRecorder.recordAndReturnCount(provider.remainingBytes());
   }
 }
