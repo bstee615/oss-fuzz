8f77dabcd57c45bba24c6fbf971d800dbe392ef0
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2023-01-19 12:46:38 -0600

    Run all projects in do_one_project

---
+++ run_all.sh
@@ -0,0 +1,7 @@
+while read p
+do
+    PROJECT=$(echo $p | cut -d " " -f 1)
+    FUZZER=$(echo $p | cut -d " " -f 2)
+    timeout 30m bash scripts/7_dual_implementation_2/do_one_project.sh $PROJECT $FUZZER &> do_one_test_${PROJECT}_${FUZZER}.log
+    echo $p
+done < <(head -n 10 data/1_preprocess/java-projects-fuzzers-from-csv.txt) | tqdm --total 10 --desc running...
