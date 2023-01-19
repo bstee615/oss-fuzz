5cffb50e37e553c1d60d9702c79ee5d612aa4995
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2023-01-18 22:26:55 -0600

    Update display log

--- scripts/0_misc/display.py
+++ scripts/0_misc/display.py
@@ -3,9 +3,11 @@ Display examples in human readable format.
 """
 import jsonlines
 
-with jsonlines.open("head100.jsonl") as reader:
-    reader = list(reader)[:1]
-    for example in reader:
-        lines_covered = list(sorted(set(e["relative_lineno"] for e in example["lines_covered"])))
+with jsonlines.open("head1.json") as reader:
+    # reader = list(reader)[:1]
+    for i, example in enumerate(reader):
+        lines_covered = list(e["relative_lineno"] for e in example["lines_covered"])
+        print("EXAMPLE:", i, example["project"], example["class"], example["method"])
+        print("\n".join(["\t" + v["name"] + "=" + str(v["text"]) for v in example["entry_variables"] if v["tag"] == "variable"]))
         print("COVERED:", lines_covered)
-        print("\n".join(str(i).rjust(3, " ") + " " + l for i, l in enumerate(example["code"].splitlines())))
+        print("\n".join(str(i).rjust(3, " ") + " " + l + (" // TRACED" if i in lines_covered else "") for i, l in enumerate(example["code"].splitlines(), start=1)))
