73eee3fa73550173bf8100b2e44fb16b7c7a6fc4
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2023-01-19 23:02:45 -0600

    Clean up build/run all scripts

--- build_all_fuzzers.sh
+++ build_all_fuzzers.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 function getData() {
-    # cat data/1_preprocess/java-projects-from-csv.txt
-    head -n1 data/1_preprocess/java-projects-from-csv.txt
+    cat data/1_preprocess/java-projects-from-csv.txt
+    # head -n1 data/1_preprocess/java-projects-from-csv.txt
 }
 while read p
 do
-    echo BUILDING PROJECT $p... | tee -a build_all_fuzzers.log
+    echo BEGIN BUILDING PROJECT $p... | tee -a build_all_fuzzers.log
     git checkout projects/$p &>> build_all_fuzzers.log
     python scripts/7_dual_implementation_2/modify_build_scripts.py $p &>> build_all_fuzzers.log
-    bash scripts/0_misc/build_project_fuzzer.sh $p --no_cache_build_image &>> build_all_fuzzers.log
+    (bash scripts/0_misc/build_project_fuzzer.sh $p --no_cache_build_image || echo $p >> build_failed.log) &>> build_all_fuzzers.log
+    git checkout projects/$p &>> build_all_fuzzers.log
+    echo DONE BUILDING PROJECT $p... >> build_all_fuzzers.log
 done < <(getData) | tqdm --total $(getData | wc -l)
--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,14 @@
+#!/bin/bash
+
+LOG_DIR="do_one_test_logs"
+mkdir -p $LOG_DIR
+
+DATA_FILE="data/1_preprocess/java-projects-fuzzers-from-csv.txt"
+
 while read p
 do
     PROJECT=$(echo $p | cut -d " " -f 1)
     FUZZER=$(echo $p | cut -d " " -f 2)
-    timeout 30m bash scripts/7_dual_implementation_2/do_one_project.sh $PROJECT $FUZZER &> do_one_test_${PROJECT}_${FUZZER}.log
+    timeout 30m bash scripts/7_dual_implementation_2/do_one_project.sh $PROJECT $FUZZER &> $LOG_DIR/${PROJECT}_${FUZZER}.log
     echo $p
-# done < <(head -n 10 data/1_preprocess/java-projects-fuzzers-from-csv.txt) | tqdm --total 10 --desc running...
-done < data/1_preprocess/java-projects-fuzzers-from-csv.txt | tqdm --total $(cat data/1_preprocess/java-projects-fuzzers-from-csv.txt | wc -l) --desc running...
+done < $DATA_FILE | tqdm --total $(cat $DATA_FILE | wc -l)
