ec74345722f417f754596de849df5c40e36be349
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2023-01-26 10:56:33 -0600

    Echo exit code of build_fuzzers

--- scripts/0_misc/build_project_fuzzer.sh
+++ scripts/0_misc/build_project_fuzzer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PROJECT="$1"
 shift
-python3 infra/helper.py build_fuzzers --sanitizer address --architecture x86_64 $PROJECT $@
+exec python3 infra/helper.py build_fuzzers --sanitizer address --architecture x86_64 $PROJECT $@
